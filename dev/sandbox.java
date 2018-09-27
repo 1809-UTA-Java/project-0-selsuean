acbdf7b6abdf3f27e922dac3fca3568ef2470282
selsuean <selsuean@gmail.com>
2018-09-27 08:59:10 -0700

    sep26 change

--- dev/sandbox.java
+++ dev/sandbox.java
@@ -4,7 +4,20 @@ package dev;
 
 public class sandbox {
 
-	
+	public static void login() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Are you a new user? Y/N");
+		Char ans = sc.next();
+		System.out.println(ans);
+		
+		if (ans == 'Y' || ans == 'y') {
+			//go to newUser();
+		}
+		
+		else if (ans == 'N' || ans == 'n') {
+			//go to logIn();
+		}
+	}
 
 
 	public static void main(String args[]) {
