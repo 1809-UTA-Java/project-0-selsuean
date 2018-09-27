5eff5e855b58d1728922453328d48f9298495a52
selsuean <selsuean@gmail.com>
2018-09-26 21:13:44 -0500

    first changes, scrap code

---
+++ dev/layout.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ * Class Person
+ *      name
+ *      username
+ *      password
+ * 
+ * 
+ *      address
+ *      age
+ *      birthday
+ *      numAccount
+ *      permission - customer, employee, admin
+ *   
+ *       
+ *      openApplication
+ *      
+ * Customer extends Person
+ * 
+ * Employee extends Person
+ * 
+ * Admin extends Person 
+ * 
+ *      
+ * Class Account
+ *      account number 
+ *      
+ */
+
+
+public interface layout {
+
+public class accountInfo {
+    //private variables means no way for other
+    //classes to retrieve or modify these values
+    //directly
+
+    //maybe make them protected so subclasses can still have 
+    //access to the value? 
+    private double checkingsAmount;
+    private double savingsAmount;
+
+    //the following public methods make the
+    //values available outside of this class 
+    public double getCheckings() {
+        return this.checkingsAmount;
+    }
+
+    public double getSavings() {
+        return this.savingsAmount;
+    }
+
+}
+    
+}
--- dev/sandbox.java
+++ dev/sandbox.java
@@ -1,11 +1,14 @@
-# sandbox
+// sandbox
 
 package dev;
 
 public class sandbox {
 
+	
+
+
 	public static void main(String args[]) {
-		//TODO: sandbox area for test code; see how requirements all fit together 
+		System.out.println("placeholder text."); 
 	}	
 
 }
