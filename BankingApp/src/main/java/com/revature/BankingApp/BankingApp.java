d3a1ec04171fd4bbcb8a2aed29220e3f2ee1f77f
selsuean <selsuean@gmail.com>
2018-10-03 16:16:27 -0500

    fixed merge confl

--- BankingApp/src/main/java/com/revature/BankingApp/BankingApp.java
+++ BankingApp/src/main/java/com/revature/BankingApp/BankingApp.java
@@ -1,30 +1,16 @@
 package com.revature.BankingApp;
 
 import java.util.ArrayList;
-<<<<<<< HEAD
-=======
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
->>>>>>> 6125969c88a3ae3456a9e600ddc9d00aa89b1e18
 import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
-<<<<<<< HEAD
 public class BankingApp {
 	// arraylist of user objects
 	static ArrayList<User> userList = new ArrayList<User>();
 
-=======
-public class BankingApp 
-{
-	//arraylist of user objects 
-	static ArrayList<User> userList = new ArrayList<User>();
-	
->>>>>>> 6125969c88a3ae3456a9e600ddc9d00aa89b1e18
 	public static void login() {
 
 		// new user input reader
@@ -39,13 +25,8 @@ public class BankingApp
 		if (ans.equalsIgnoreCase("y")) {
 			newUser(sc);
 		}
-<<<<<<< HEAD
 
 		// if not, goes to searchUser page
-=======
-		
-		//if not, goes to searchUser page
->>>>>>> 6125969c88a3ae3456a9e600ddc9d00aa89b1e18
 		else if (ans.equalsIgnoreCase("n")) {
 			searchUser(sc);
 		}
@@ -57,7 +38,6 @@ public class BankingApp
 			login();
 		}
 	}
-<<<<<<< HEAD
 
 	// takes username and compares it to the user.username field in the user object
 	// stored
@@ -132,76 +112,6 @@ public class BankingApp
 		User newUser = new User();
 
 		// adds User instance to arrayList
-=======
-	
-	
-	// takes username and compares it to the user.username field in the user object stored
-	// in an arraylist
-	public static void searchUser(Scanner sc) {
-		String strU = null; 
-		String strP = null;
-		
-		System.out.println("Please login. \n");
-		System.out.println("Enter your username: ");
-		String storeUser = sc.nextLine();
-		
-		System.out.println("Type your password: ");
-		String storePW = sc.nextLine();
-		
-		for (User obj : userList) {
-			strU = ((Person) obj).getUsername();
-			strP = ((Person) obj).getPassword();
-			
-			if (strU.equals(storeUser) && strP.equals(storePW)) {
-				actionPage(obj, sc);
-			}
-			else {
-				System.out.println("Incorrect login. Try again.");
-				searchUser(sc);
-			}
-		}	
-	}
-	
-	
-	public static void actionPage(User currUser, Scanner sc) {
-		
-		System.out.println("Welcome to THE BANK.\n");
-		
-		System.out.println("What would you like to do?\n "
-				+ "1 - View user information.\n"
-				+ "2 - View account information.\n"
-				+ "3 - Apply to create an account.\n"
-				+ "4 - Apply to a joint account.");
-		
-		int ans = sc.nextInt();
-		sc.nextLine();
-		
-		if (ans == 1) {
-			// TODO: display User information
-		}
-		else if (ans == 2) {
-			// TODO: Which account
-		}
-		else if (ans == 3) {
-			// TODO: create application  
-		}
-		else if (ans == 4) {
-			// TODO: create application  
-		}		
-	}
-	
-	
-	public static void actionMoney(User currUser, Scanner sc) {
-		
-	}
-	
-	
-	public static void newUser(Scanner sc) {
-		//creates new User object
-		User newUser = new User();
-		
-		//adds User instance to arrayList
->>>>>>> 6125969c88a3ae3456a9e600ddc9d00aa89b1e18
 		userList.add(newUser);
 
 		// gets name
@@ -215,7 +125,6 @@ public class BankingApp
 		// gets username; if there are spaces, removes spaces from string
 		System.out.println("Enter your username:");
 		String makeUsername = sc.nextLine();
-<<<<<<< HEAD
 		makeUsername = makeUsername.replaceAll("\\s", "");
 
 		boolean availCheck = checkAvailability(makeUsername);
@@ -267,22 +176,6 @@ public class BankingApp
 		return avail;
 	}
 
-=======
-		makeUsername = makeUsername.replaceAll("\\s","");
-		
-		//TODO: check for username availability 
-		
-		System.out.print("Your username is: " + makeUsername + ". \nIf there were any whitespaces included in your entry, "
-				+ "they have been removed.\n");
-		
-		//sets User instance username 
-		newUser.username = makeUsername;
-		
-		createPW(sc, newUser);
-	}
-	
-	
->>>>>>> 6125969c88a3ae3456a9e600ddc9d00aa89b1e18
 	public static void createPW(Scanner sc, Person user) {
 		System.out.println("Enter your password: ");
 		String makePW = sc.nextLine();
@@ -293,26 +186,13 @@ public class BankingApp
 		if (makePW.equals(pwCheck)) {
 			user.password = makePW;
 			searchUser(sc);
-<<<<<<< HEAD
 		} else {
-=======
-		}
-		else {
->>>>>>> 6125969c88a3ae3456a9e600ddc9d00aa89b1e18
 			System.out.println("Password does not match. Please try again.");
 			createPW(sc, user);
 		}
 	}
-<<<<<<< HEAD
 
 	public static void main(String[] args) {
 		login();
 	}
-=======
-	
-	
-    public static void main( String[] args)    {
-    	login();
-    }
->>>>>>> 6125969c88a3ae3456a9e600ddc9d00aa89b1e18
 }
